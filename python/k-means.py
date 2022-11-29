bf71428efbc2c6126924fe234f8b4008c0e905a2
Mika Väliaho <mika.valiaho@gmail.com>
2022-11-29 15:51:10 +0200

    päivä pulkassa

--- python/k-means.py
+++ python/k-means.py
@@ -22,45 +22,53 @@ class dataHandler:
         dataMatrix[:,2] = s.data[2::3]
         return dataMatrix
         
-    def dataShow(s):
+    def dataShow(s,node):
         fig = plt.figure()
         ax = fig.add_subplot(projection='3d')
         ax.scatter(s.dataX, s.dataY, s.dataZ)
+        ax.scatter(node[0], node[1], node[2], color="red", marker="*")
         ax.set_xlabel('X Label')
         ax.set_ylabel('Y Label')
         ax.set_zlabel('Z Label')
         plt.show()        
 class kMeans:
     def __init__(s,data):
-        s.data = data
+        s.measureData = data
         s.min = np.min(data)
         s.max = np.max(data)
-        s.nodeMatrix = np.zeros((4,3))
+        s.node = np.zeros((4,4))
     def randomNode(s):
-        s.nodeMatrix = np.random.randint(s.min,s.max,(4,3))
-        print("Node matrix len",len(s.nodeMatrix))
+        s.node = np.random.randint(s.min,s.max,(4,3))
+        print("Nodet arvottu")
         
     def vectorLength(s):
-        distance = np.zeros(len(s.nodeMatrix))
-        avg = np.zeros((len(s.nodeMatrix),4))
-        
-        for x in range(len(s.data)):
-            # print("Rivi :", x)
-            for y in range(len(s.nodeMatrix)):
-                # print("Node :", y)  
-                distance[y] = np.abs(np.sqrt(np.power((s.data[x,0] - s.nodeMatrix[y,0]),2)+ np.power((s.data[x,1] - s.nodeMatrix[y,1]),2) + np.power((s.data[x,2] - s.nodeMatrix[y,2]),2)))
-            i = np.argmin(distance)
-            avg[i,3] += 1
-            avg[i,0:3] += s.data[x,0:3] 
+        while(True):
+            s.randomNode()
+            distance = np.zeros(len(s.node))
+            avg = np.zeros((len(s.node),4))
+            for x in range(len(s.measureData)):
+                # print("Rivi :", x)
+                for y in range(len(s.node)):
+                    # print("Node :", y)  
+                    distance[y] = np.abs(np.sqrt(np.power((s.measureData[x,0] - s.node[y,0]),2)+ np.power((s.measureData[x,1] - s.node[y,1]),2) + np.power((s.measureData[x,2] - s.node[y,2]),2)))
+                i = np.argmin(distance)
+                avg[i,3] += 1
+                avg[i,0:3] += s.measureData[x,0:3]
+            if not np.min(avg[:,3]) == 0:
+                break   
         for i in range(len(avg)):
             for j in range(4):
-                avg[i] = avg[i,:] / avg[i,3]     
-        print(avg)  
+                avg[i] = avg[i,:] / avg[i,3] 
+        s.node = avg
+    def getValues(s):
+        s.node = s.node[:,0:3]
+        return s.node
+
             
                 
 if __name__ == "__main__":
     df = dataHandler("python\putty.log")
     kMeans = kMeans(df.handleData())
-    kMeans.randomNode()
     kMeans.vectorLength()
-    # df.dataShow()
+    nodes = kMeans.getValues()
+    df.dataShow(nodes)
