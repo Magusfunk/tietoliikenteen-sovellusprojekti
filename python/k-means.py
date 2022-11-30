5220362404047be7c7fe943a01c9128a941aee4d
Mika Väliaho <mika.valiaho@gmail.com>
2022-11-30 11:57:03 +0200

    refaktoroitu koodia

--- python/k-means.py
+++ python/k-means.py
@@ -1,74 +1,75 @@
 import numpy as np
 import matplotlib.pyplot as plt
-class dataHandler:
+class kMeans:
     def __init__(s,file):
-        s.file = file
-        s.__data = ""
-        s.loadData()
-        
-    def loadData(s):
-        s.data = np.loadtxt(s.file)
-        print(s.data)
-        s.handleData()
-        
-    def handleData(s): 
-        numberOfRows = int(len(s.data) / 3)       
-        dataMatrix = np.zeros((numberOfRows,3)) 
+        s.data = ""
+        s.min = 0
+        s.max = 0        
+        s.inputMatrix = np.zeros((4,4))
+        s.outputMatrix = np.zeros((4,4))
+        s.handleData(file)
+               
+    def handleData(s,file):
+        s.data = np.loadtxt(file)
+        s.numberOfRows = int(len(s.data) / 3) 
+        dataMatrix = np.zeros((s.numberOfRows,3))
+        s.kMeansCount = 4 
         s.dataX = s.data[0::3]
         s.dataY = s.data[1::3]
         s.dataZ = s.data[2::3] 
         dataMatrix[:,0] = s.data[0::3]
         dataMatrix[:,1] = s.data[1::3]
         dataMatrix[:,2] = s.data[2::3]
-        return dataMatrix
-        
-    def dataShow(s,node):
-        fig = plt.figure()
-        ax = fig.add_subplot(projection='3d')
-        ax.scatter(s.dataX, s.dataY, s.dataZ)
-        ax.scatter(node[0], node[1], node[2], color="red", marker="*")
-        ax.set_xlabel('X Label')
-        ax.set_ylabel('Y Label')
-        ax.set_zlabel('Z Label')
-        plt.show()        
-class kMeans:
-    def __init__(s,data):
-        s.measureData = data
-        s.min = np.min(data)
-        s.max = np.max(data)
-        s.node = np.zeros((4,4))
+        s.min = np.min(s.data)
+        s.max = np.max(s.data)
+        s.measureData = dataMatrix
+
     def randomNode(s):
-        s.node = np.random.randint(s.min,s.max,(4,3))
-        print("Nodet arvottu")
+        randomMatrix = np.random.randint(s.min,s.max,(4,3))
+        return randomMatrix
         
-    def vectorLength(s):
-        while(True):
-            s.randomNode()
-            distance = np.zeros(len(s.node))
-            avg = np.zeros((len(s.node),4))
+    def kMeans(s):
+        
+        avg = s.vectorLenghtCalculate()
+        s.outputMatrix = avg
+            
+    def vectorLenghtCalculate(s):            
+        avg = np.zeros((s.kMeansCount,4))
+        distance = np.zeros(s.kMeansCount)
+        s.inputMatrix = s.randomNode()
+        while True:
             for x in range(len(s.measureData)):
                 # print("Rivi :", x)
-                for y in range(len(s.node)):
+                for y in range(len(s.inputMatrix)):
                     # print("Node :", y)  
-                    distance[y] = np.abs(np.sqrt(np.power((s.measureData[x,0] - s.node[y,0]),2)+ np.power((s.measureData[x,1] - s.node[y,1]),2) + np.power((s.measureData[x,2] - s.node[y,2]),2)))
+                    distance[y] = np.abs(np.sqrt(np.power((s.measureData[x,0] - s.inputMatrix[y,0]),2)+ np.power((s.measureData[x,1] - s.inputMatrix[y,1]),2) + np.power((s.measureData[x,2] - s.inputMatrix[y,2]),2)))
                 i = np.argmin(distance)
                 avg[i,3] += 1
                 avg[i,0:3] += s.measureData[x,0:3]
-            if not np.min(avg[:,3]) == 0:
-                break   
+            print(avg)
+            if np.min(avg[:,3] == 0.0):
+                print("Arvottu uusi")
+                s.inputMatrix = s.randomNode()
+            if not np.min(avg[:,3] == 0.0):
+                print(np.min(avg[:,3]))
+                print("Looppi katkaistu")
+                break        
         for i in range(len(avg)):
             for j in range(4):
                 avg[i] = avg[i,:] / avg[i,3] 
-        s.node = avg
-    def getValues(s):
-        s.node = s.node[:,0:3]
-        return s.node
-
-            
+        return avg
+    
+    def dataShow(s):
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+        ax.scatter(s.measureData[:,0], s.measureData[:,1], s.measureData[:,2])
+        ax.scatter(s.outputMatrix[:,0], s.outputMatrix[:,1], s.outputMatrix[:,2], color="red", marker="*")
+        ax.set_xlabel('X Label')
+        ax.set_ylabel('Y Label')
+        ax.set_zlabel('Z Label')
+        plt.show()   
                 
 if __name__ == "__main__":
-    df = dataHandler("python\putty.log")
-    kMeans = kMeans(df.handleData())
-    kMeans.vectorLength()
-    nodes = kMeans.getValues()
-    df.dataShow(nodes)
+    kMeans = kMeans("python\putty.log")
+    kMeans.kMeans()
+    kMeans.dataShow()
